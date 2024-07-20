141f1409a01ef3d8ff3e2d4dfc46d4e5bd318a22
Kartikay Sinha <118372323+KS1745@users.noreply.github.com>
2024-07-20 23:57:15 +0530

    Time: 1640 ms (5.15%) | Memory: 27 MB (38.06%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        //Brute Force
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            int count = 0;
+            for(int j=0; j<n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+
+            if(count>(n/2))
+                return nums[i];
+            
+        }
+        return -1;
+    }
+};
