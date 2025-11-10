02fb0c44e74a5a9d3a12e08f5d6cb84a4c2e92b6
Kartikay Sinha <118372323+KS1745@users.noreply.github.com>
2025-11-10 08:08:33 -0500

    Time: 4 ms (89.80%) | Memory: 63.2 MB (83.62%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    let xCopy = x;
+
+    let rev = 0;
+
+    while ( x > 0) {
+        let rem = x % 10;
+            rev = rev*10 + rem;
+            x = Math.floor(x/10);
+    }
+    
+    if(rev == xCopy) {
+        return true;
+    } else {
+        return false;
+    }
+};
