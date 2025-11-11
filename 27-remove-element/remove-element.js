ee23fe386adf4596715c0c2f057158e2b20be6ac
Kartikay Sinha <118372323+KS1745@users.noreply.github.com>
2025-11-11 10:45:02 -0500

    Time: 0 ms (100.00%) | Memory: 54.3 MB (52.91%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+
+    let x = 0;
+    for(let i=0; i<nums.length; i++) {
+        //shift element to left if it is not equal to val
+        if(nums[i] !== val) {
+            nums[x] = nums[i];
+            x = x + 1;
+
+        }
+    }
+
+    return x;
+    
+};
