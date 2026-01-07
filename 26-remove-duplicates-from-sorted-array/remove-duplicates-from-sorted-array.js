cf4f60827f8751c769c56da29c8f3852516c87b0
Kartikay Sinha <118372323+KS1745@users.noreply.github.com>
2026-01-07 14:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 58.5 MB (22.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-
     let x = 0;
-
     for(let i=0; i<nums.length; i++) {
-        
         if(nums[i] > nums[x]) {
             x = x + 1;
             nums[x] = nums[i];
         }
     }
 
-    return x+1;
-    
+    return (x+1);
 
+    
 };
